20bce1204778b6c6845f842bfc68dc4e90f78a88
BuildTools <arthurpisakhov1@gmail.com>
2019-08-31 23:04:34 -0400

    stretch 1

--- src/App.js
+++ src/App.js
@@ -1,55 +1,15 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, {useState} from "react";
+import React from "react";
 import "./App.css";
-import BottomRow from "./BottomRow";
+import TopRow from './TopRow.js';
+
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-  const [home, setHome] = useState(6);
-  const [away, setAway] = useState(18);
-
-  const onClick = (e) => {
-    if (e.target.classList.contains('homeButtons__touchdown')) {
-      setHome(home + 7);
-    } else if (e.target.classList.contains('homeButtons__fieldGoal')) {
-      setHome(home + 3)
-    } else if (e.target.classList.contains('awayButtons__touchdown')) {
-      setAway(away + 7);
-    } else if (e.target.classList.contains('awayButtons__fieldGoal')) {
-      setAway(away + 3)
-    }
-  }
 
   return (
     <div className="container">
-      <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
-
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
-            <div className="home__score">{home}</div>
-          </div>
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{away}</div>
-          </div>
-        </div>
-        <BottomRow />
-      </section>
-      <section className="buttons">
-        <div className="homeButtons">
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button onClick={onClick} className="homeButtons__touchdown">Home Touchdown</button>
-          <button onClick={onClick} className="homeButtons__fieldGoal">Home Field Goal</button>
-        </div>
-        <div className="awayButtons">
-          <button onClick={onClick} className="awayButtons__touchdown">Away Touchdown</button>
-          <button onClick={onClick} className="awayButtons__fieldGoal">Away Field Goal</button>
-        </div>
-      </section>
+      <TopRow />
     </div>
   );
 }
---
+++ src/TopRow.js
@@ -0,0 +1,50 @@
+import React, {useState} from "react";
+import "./App.css";
+import BottomRow from "./BottomRow";
+
+const TopRow = () => {
+  const [home, setHome] = useState(6);
+  const [away, setAway] = useState(18);
+
+  const onClick = (e) => {
+    if (e.target.classList.contains('homeButtons__touchdown')) {
+      setHome(home + 7);
+    } else if (e.target.classList.contains('homeButtons__fieldGoal')) {
+      setHome(home + 3)
+    } else if (e.target.classList.contains('awayButtons__touchdown')) {
+      setAway(away + 7);
+    } else if (e.target.classList.contains('awayButtons__fieldGoal')) {
+      setAway(away + 3)
+    }
+  }
+  return (
+    <div className="container">
+      <section className="scoreboard">
+        <div className="topRow">
+          <div className="home">
+            <h2 className="home__name">Lions</h2>
+            <div className="home__score">{home}</div>
+          </div>
+          <div className="timer">00:03</div>
+          <div className="away">
+            <h2 className="away__name">Tigers</h2>
+            <div className="away__score">{away}</div>
+          </div>
+        </div>
+        <BottomRow />
+      </section>
+      <section className="buttons">
+        <div className="homeButtons">
+          <button onClick={onClick} className="homeButtons__touchdown">Home Touchdown</button>
+          <button onClick={onClick} className="homeButtons__fieldGoal">Home Field Goal</button>
+        </div>
+        <div className="awayButtons">
+          <button onClick={onClick} className="awayButtons__touchdown">Away Touchdown</button>
+          <button onClick={onClick} className="awayButtons__fieldGoal">Away Field Goal</button>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default TopRow;
